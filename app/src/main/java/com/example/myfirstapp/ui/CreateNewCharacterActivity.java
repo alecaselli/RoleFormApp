105468b7ef882ec8fec3b35b4a13ae308f887987
alecaselli <cas1312@ymail.com>
2020-09-13 06:57:35 +0200

    v1.04

--- app/src/main/java/com/example/myfirstapp/ui/CreateNewCharacterActivity.java
+++ app/src/main/java/com/example/myfirstapp/ui/CreateNewCharacterActivity.java
@@ -12,5 +12,7 @@ public class CreateNewCharacterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_character);
+
+
     }
 }
